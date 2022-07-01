3e0d2946ceb1088471ddf3750dd403c852401bfc
ankit-kumar-12-12 <96494631+ankit-kumar-12-12@users.noreply.github.com>
2022-07-01 19:40:19 +0530

    Add files via upload

---
+++ AKKI's COMPUTERS/src/Project/ConnectionProvider.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+
+/**
+ *
+ * @author LAPPI_2.O
+ */
+public class ConnectionProvider 
+{
+    
+    public static Connection getcon()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/akkis_db","root","");
+            
+            System.out.println("andar");
+            
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+            
+    }
+}
